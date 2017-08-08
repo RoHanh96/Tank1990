756bf5cefaa2ee54d7df8ba1cbd4f066b2c30449
RoHanh96 <hanhnv.bkhn@gmail.com>
2017-08-08 12:00:13 +0700

    enemy_move

---
+++ Assets/Scripts/BulletEnemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletEnemy : MonoBehaviour {
+
+	// Use this for initialization
+	public int speed;
+	public float lifeTime;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D> ().velocity = transform.up.normalized * speed;
+		Destroy (gameObject, lifeTime);
+	}
+}
